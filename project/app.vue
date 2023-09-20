2245e21b32839caab79a028082761cb3ce3a70b8
Chelsea Yuan <70819287+chelsealyuan@users.noreply.github.com>
2023-09-20 11:56:02 -0500

    created nuxt project + tested some Vue components

---
+++ project/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <Welcome/>
+    <Card />
+  </div>
+</template>
---
+++ project/components/Welcome.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="container">
+
+        <h1>This is a Custom Welcome component!</h1>
+    </div>
+</template>
