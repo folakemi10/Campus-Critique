e136f493c9fd1ce5cd1d6454d91a6ee5f75f5540
Chelsea Yuan <70819287+chelsealyuan@users.noreply.github.com>
2023-10-15 13:08:19 -0500

    registering adds auth user to database

--- project/lib/db.ts
+++ project/lib/db.ts
@@ -4,67 +4,70 @@ https://github.com/razorcx-courses/nuxt3-firestore-public/blob/108d4248c97ab4e8e
 */
 
 import {
-    collection,
-    getDocs,
-    getDoc,
-    addDoc,
-    deleteDoc,
-    doc,
-    query,
-    where,
-    setDoc,
-    collectionGroup,
-    Timestamp
-  } from "firebase/firestore";
-  import { db } from "./firebase";
+  collection,
+  getDocs,
+  getDoc,
+  addDoc,
+  deleteDoc,
+  doc,
+  query,
+  where,
+  setDoc,
+  collectionGroup,
+  Timestamp,
+} from "firebase/firestore";
+import { db } from "./firebase";
 
-  export async function queryEntireCollection(col: string) {
-    // @ts-ignore
-    const colRef = collection(db, col);
-  
-    const snapshot = await getDocs(colRef);
-  
-    const docs = Array.from(snapshot.docs).map((doc) => {
-      return {
-        ...doc.data(),
-        id: doc.id,
-      };
-    });
-  
-    return docs;
-  }
+export async function queryEntireCollection(col: string) {
+  // @ts-ignore
+  const colRef = collection(db, col);
 
-  export async function queryCollectionByField(col: string, field: string, value: Object) {
-    const colRef = collection(db, col);
-    
-    const q = query(colRef, where(field, "==", value));
-    const docs = await getDocs(q);
-    
-    const result = Array.from(docs.docs).map((doc) => {
-      return {
-        ...doc.data(),
-        id: doc.id,
-      };
-    });
-  
-    return result;
-  }
-  
-  
-  export async function set(col: string, document: Object) {
-    await setDoc(doc(collection(db, col)), document, { merge: true });
-  }
-  
-  export async function add(col: string, document: Object) {
-    // @ts-ignore
-    const colRef = collection(db, col);
-  
-    const docRef = await addDoc(colRef, document);
-  
-    return docRef;
-  }
-  
-  export async function del(col: string, id: string) {
-    const docRef = doc(db, col, id);
-    return await deleteDoc(docRef);
-  }
+  const snapshot = await getDocs(colRef);
+
+  const docs = Array.from(snapshot.docs).map((doc) => {
+    return {
+      ...doc.data(),
+      id: doc.id,
+    };
+  });
+
+  return docs;
+}
+
+export async function queryCollectionByField(
+  col: string,
+  field: string,
+  value: Object
+) {
+  const colRef = collection(db, col);
+
+  const q = query(colRef, where(field, "==", value));
+  const docs = await getDocs(q);
+
+  const result = Array.from(docs.docs).map((doc) => {
+    return {
+      ...doc.data(),
+      id: doc.id,
+    };
+  });
+
+  return result;
+}
+
+export async function set(col: string, document: Object) {
+  await setDoc(doc(collection(db, col)), document, { merge: true });
+}
+
+export async function add(col: string, document: Object) {
+  // @ts-ignore
+  const colRef = collection(db, col);
+
+  const docRef = await addDoc(colRef, document);
+
+  return docRef;
+}
+
+export async function del(col: string, id: string) {
+  const docRef = doc(db, col, id);
+  return await deleteDoc(docRef);
+}
