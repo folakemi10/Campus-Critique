0b3c45dbdbe6a819813198af26422178b2a8ccd0
folakemi <folakemishofu@gmail.com>
2023-10-16 03:29:52 -0500

    profile functional

--- project/pages/profile.vue
+++ project/pages/profile.vue
@@ -1,21 +1,55 @@
 <template>
-    <div>
-        Profile
-    </div>
+  <v-container class="flex-vertical justify-center">
+    <router-link to="/">
+      <v-btn>
+        return
+      </v-btn>
+    </router-link>
 
-    <v-card>
-        {{ text }}
+    <v-card class="mx-10	my-10 ">
+      <v-card-text>
+        <h1 class="text-3xl font-semibold mb-4"> {{ getUsername }} </h1>
+        <div class="text-lg mb-4">
+          Number of Reviews: {{ filteredPosts.length }}
+        </div>
+      </v-card-text>
     </v-card>
-</template>
-
-<script>
-export default {
     
-    props: ['text']
+    <Card v-for="(review, index) in filteredPosts" :key="index" :review="review"></Card>
+  </v-container>
+</template>
+  
+<script>
+import { queryEntireCollection } from "~/lib/db"; // Replace with your Firebase package import
 
+export default {
+  data() {
+    return {
+      allPosts: [],
+      firebaseUser: null
+    }
+  },
+  async mounted() {
+    this.firebaseUser = useFirebaseUser();;
+    this.allPosts = await queryEntireCollection("posts");
+  },
+  computed: {
+    filteredPosts() {
+      if (!this.firebaseUser || !this.allPosts || this.allPosts.length === 0) {
+        return [];
+      }
+      const userId = this.firebaseUser.uid;
+      return this.allPosts.filter((post) => post.uid === userId);
+    },
+    getUsername() {
+      if (!this.firebaseUser || !this.allPosts || this.allPosts.length === 0) {
+        return "Bad Registeration. Delete user from database and register again";
+      }
+      const firebaseUser = useFirebaseUser();
+      const username = firebaseUser.value.displayName;
+      return username;
+    }
+  }
 }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
+  
