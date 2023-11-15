a03637b37d2c74b787d123b00514a6ff59726751
folakemi <folakemishofu@gmail.com>
2023-11-14 19:29:24 -0600

    changes

--- project/pages/friendsProfile.vue
+++ project/pages/friendsProfile.vue
@@ -12,30 +12,16 @@
 
             <v-window-item value="tab-0">
                 <v-container class="flex flex-col items-center justify-center">
-                    <v-card v-if="isFromFriendsPage" class="min-w-full max-w-xl">
+                    <v-card class="min-w-full max-w-xl">
                         <h1 class="text-3xl font-semibold mt-4 mb-4 pl-4 pr-4"> {{ friendName ? friendName.firstname :
-                            'Loading...' }} {{ friendName
-        ? friendName.lastname : 'Loading...' }}</h1>
-                        <v-btn :variant="variant" class="mx-4 mb-4" @click="inviteFriend">Invite Friend</v-btn>
+                            'Loading...' }} {{ friendName ? friendName.lastname : 'Loading...' }}</h1>
+                        <v-btn v-if="isFromFriendsPage" :variant="variant" class="mx-4 mb-4" @click="inviteFriend">Invite Friend</v-btn>
                     </v-card>
-                    <v-card v-else class="min-w-full max-w-xl ">
-                        <v-card-text>
-                            <h1 class="text-3xl font-semibold mb-4"> {{ userName.firstname }} {{ userName.lastname }}</h1>
-                            <div class="text-lg mb-4">
-                                Number of Reviews: {{ allPosts ? allPosts.length : 'Loading...' }}
-                            </div>
-                            <LogoutBtn />
-                        </v-card-text>
-                    </v-card>
-
                 </v-container>
 
-
-                <Card class="min-w-full max-w-xl" v-if="isFromAcceptedsPage" v-for="(review, index) in friendPosts"
+                <Card class="min-w-full max-w-xl" v-if="isFromAcceptedPage" v-for="(review, index) in friendPosts"
                     :key="review.id" :review="review" :showChangeBtns="true">
                 </Card>
-
-
 
             </v-window-item>
             <v-window-item value="tab-1">
@@ -56,7 +42,6 @@ import { useRoute } from 'vue-router';
 const firebaseUser = useFirebaseUser();
 const userId = firebaseUser.value?.uid;
 let friendId = "";
-const allPosts = ref();
 const friendPosts = ref();
 const userName = ref();
 const friendName = ref();
@@ -68,59 +53,31 @@ const snackbarText = ref();
 const props = defineProps({
     variant: {
         type: String as PropType<"outlined" | "flat" | "text" | "elevated" | "tonal" | "plain">,
-        default: "outlined", // Default to a valid Vuetify variant
+        default: "outlined", 
     },
 });
 
 //need to conditionaly render page
 const isFromFriendsPage = route.query.fromFriendsPage === 'fromFriendsPage';
-const isFromAcceptedsPage = route.query.fromFriendsPage === 'fromAcceptedPage';
+const isFromAcceptedPage = route.query.fromFriendsPage === 'fromAcceptedPage';
 const invitationsRef = collection(db, 'friends');
 
-onMounted(async () => {
-  if (userId) {
-    allPosts.value = await queryCollectionByField("posts", "uid", userId);
-  } else {
-    console.log('userId does not exist');
-  }
-});
 
-onMounted(async () => {
-  if (friendId) {
-    friendPosts.value = await queryCollectionByField("posts", "uid", friendId);
-  } else {
-    console.log('friendId does not exist');
-  }
-});
 const usersRef = collection(db, "users");
 const q = query(usersRef, where("uid", "==", userId));
 const querySnapshot = await getDocs(q);
 querySnapshot.forEach((doc) => {
-    // doc.data() is never undefined for query doc snapshots
     userName.value = doc.data();
-
-    //console.log("in" +  userName.firstname);
 });
 
 
-definePageMeta({
-    middleware: function (to, from) {
-        const user = useFirebaseUser();
-
-
-        if (!user.value) {
-            return navigateTo('/');
-        }
-    },
-});
 
 //Control the sections of the profile page
 const tab = ref('tab-0');
 
 const tabItems = [
-  'Posts', 'Saved Courses'
+    'Posts', 'Saved Courses'
 ];
-
 
 
 
@@ -135,6 +92,8 @@ onMounted(async () => {
         fQuerySnapshot.forEach((doc) => {
             friendName.value = doc.data();
         });
+
+        friendPosts.value = await queryCollectionByField("posts", "uid", friendId);
     }
 });
 
@@ -153,19 +112,16 @@ const inviteFriend = async () => {
         if (friendName.value) {
             const friendData = friendName.value;
 
-
             // Create a unique invitation document ID based on invitationDocId
             const invitationDocId =
                 userId < friendData.uid
                     ? `${userId}_${friendData.uid}`
                     : `${friendData.uid}_${userId}`;
-
-            // Check if an invitation with the same ID already exists
+                    
             const invitationDoc = doc(invitationsRef, invitationDocId);
             const invitationDocSnapshot = await getDoc(invitationDoc);
 
             if (!invitationDocSnapshot.exists()) {
-                // Create the invitation document with the same ID as invitationDocId
                 await setDoc(invitationDoc, {
                     senderId: userId,
                     receiverId: friendData.uid,
@@ -190,7 +146,7 @@ const inviteFriend = async () => {
         console.error('Error inviting friend:', error);
     }
 };
-//const showSnackbar = computed(() => isCurrentUser.value && snackbar.value);
+
 
 
 </script>
